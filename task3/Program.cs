eec1023992e3504944c6fb1596794d946506ceff
dima korshunov <dimon4ik142@yandex.ru>
2023-01-24 19:57:03 +0300

    Task 03

---
+++ task3/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите номер дня недели");
+string? dayofweek = Console.ReadLine();
+int numberday = Convert.ToInt32(dayofweek);
+
+bool Check(int num)
+{
+    if (num <8 && num > 0)
+    {
+        return true;
+    }
+    Console.WriteLine("Это не день недели");
+    return false;
+}
+ 
+if (Check(numberday))
+{
+    if (numberday == 6 | numberday == 7)
+    {
+        Console.WriteLine("да");
+    }
+    else Console.WriteLine("нет"); 
+}
